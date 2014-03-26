33f0fd3c7bbb3280dab5bcab0576d9031bf770fe
Raphael Zimmermann <raphi.zimmermann@gmail.com>
2014-03-26 16:20:00 +0100

    Version updated to 0.0.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
             "templates/*",
             "static/*"],
     },
-    version = "0.0.1",
+    version = "0.0.2",
     license = "MIT",
     include_package_data=True,
     zip_safe=False,
