31ef499af10d5c2ffa757e66831f51a0ba5ba820
Raphael Zimmermann <raphi.zimmermann@gmail.com>
2014-04-07 21:53:33 +0200

    Migration to the tornado framework to support websockets

---
+++ previewr/static/update.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    var updater = {
+        socket: null,
+
+        start: function() {
+            var url = "ws://" + location.host + "/update";
+            updater.socket = new WebSocket(url);
+
+            updater.socket.onmessage = function(event) {
+                updater.showMessage(event.data);
+            }
+        },
+
+        showMessage: function(message) {
+            $(".content").html(message)
+        }
+    };
+    updater.start();
+});
