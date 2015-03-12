a649469c46eae87721ed147a9cdd9234edcefc09
Raphael Zimmermann <raphi.zimmermann@gmail.com>
2015-03-12 08:23:31 +0100

    New version 0.5.0.dev

--- build.py
+++ build.py
@@ -16,7 +16,7 @@ authors = [Author("Raphael Zimmermann", "mister.norbert@gmail.com")]
 description = open('README.rst').read()
 license = "MIT"
 url = "http://raphael.li/projects/previewr/"
-version = "0.4.0"
+version = "0.5.0.dev"
 
 
 @init
